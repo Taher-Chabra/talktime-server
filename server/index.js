e4a3690a81ba1bec1c10b78cd707116b5568d9ca
Taher Chabra <redsign12345@gmail.com>
2024-11-03 21:43:07 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,53 @@
+// initialise an Express server
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const config = require('./config');
+const { videoToken } = require('./tokens');
+const { socketIOServer } = require('./socket');
+const cors = require('cors');
+
+const PORT = 5100; // port on which the server runs
+const MAX_CAPACITY = 25; // maximum capacity of the room
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cors({ origin: config.allowedURLs, methods: ['GET', 'POST'] }));
+app.use(express.json());
+
+const sendTokenResponse = (token, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(
+    JSON.stringify({
+      token: token.toJwt(),
+    })
+  );
+};
+
+// set up API routes to get access tokens from Twilio
+app.get('/video/token', (req, res) => {
+  const identity = req.query.identity;
+  const room = req.query.room;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+});
+
+app.post('/video/token', (req, res) => {
+  const identity = req.body.identity;
+  const room = req.body.room;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+});
+
+// run the socket.io server on the same port
+socketIOServer(server, MAX_CAPACITY);
+
+// server running on PORT
+server.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Server ready on http://localhost:${PORT}`);
+});
---
+++ server/requests.js
@@ -0,0 +1,132 @@
+const { url } = require('./config');
+const fetch = require('node-fetch');
+
+async function getChatSession(roomId) {
+  // fetch the chat session from the db
+  try {
+    const result = await fetch(
+      `${url.client}/api/chat-sessions?roomId=${roomId}`
+    );
+    const data = await result.json();
+    return await data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+async function addChatToSession(chat, roomId) {
+  // * save the chat session in the db
+  try {
+    const chats = await getChatSession(roomId);
+    chats.push(chat);
+    const stringifiedChats = JSON.stringify(chats);
+
+    const reqBody = {
+      roomId: roomId,
+      chats: stringifiedChats,
+    };
+
+    await fetch(`${url.client}/api/chat-sessions`, {
+      method: 'PUT',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function clearChatHistory(roomId) {
+  try {
+    const reqBody = {
+      roomId: roomId,
+      chats: JSON.stringify([]),
+    };
+
+    await fetch(`${url.client}/api/chat-sessions`, {
+      method: 'PUT',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function addRoomToUser(userId, roomId) {
+  try {
+    const response = await fetch(
+      `${url.client}/api/user-rooms?userId=${userId}`
+    );
+    const rooms = await response.json();
+
+    const item = rooms.find((r) => r.roomId === roomId);
+    if (item) {
+      item.date = new Date();
+    } else {
+      rooms.push({
+        roomId: roomId,
+        date: new Date(),
+        meetingLink: `deploy_url_link/room/${roomId}`,
+        chatLink: `deploy_url_link/room/${roomId}/chat`,
+      });
+    }
+
+    const stringifiedRooms = JSON.stringify(rooms);
+
+    const reqBody = {
+      userId: userId,
+      rooms: stringifiedRooms,
+    };
+
+    fetch(`${url.client}/api/user-rooms`, {
+      method: 'PUT',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function addUserToRoom(user, roomId) {
+  try {
+    const response = await fetch(`${url.client}/api/rooms?roomId=${roomId}`);
+    const users = await response.json();
+    const item = users.find((u) => u.id === user.id);
+    if (!item) {
+      users.push(user);
+      const stringifiedUsers = JSON.stringify(users);
+
+      const reqBody = {
+        roomId: roomId,
+        users: stringifiedUsers,
+      };
+
+      fetch(`${url.client}/api/rooms`, {
+        method: 'PUT',
+        body: JSON.stringify(reqBody),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+module.exports = {
+  getChatSession,
+  addChatToSession,
+  clearChatHistory,
+  addRoomToUser,
+  addUserToRoom,
+};
